8b07a338e5d25c2aa7a91809c99860d6145da7e3
nikita-gupta2 <nikita987dsa@gmail.com>
2026-02-21 16:39:22 +0530

    frontpage

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 console.log("javascript working");
 
 let btn = document.querySelector(".theme-btn");
-let topic = document.querySelector(".nav-topic"); 
+let topics = document.querySelectorAll(".nav-topic"); 
 let sun = document.querySelector(".sun-icon");
 let moon = document.querySelector(".moon-icon");
 let theme_btn = document.querySelector(".theme-btn");
+let sbj_info = document.querySelector(".subject-info");
+let choice_btns = document.querySelectorAll(".choice");
 
 function darktheme(){
     console.log("dark theme");
@@ -20,11 +22,16 @@ function lighttheme(){
 
 btn.addEventListener("click",()=>{
     document.body.classList.toggle("dark-theme");
-    topic.classList.toggle("topic-dark");
+    topics.forEach(topic => {
+        topic.classList.toggle("topic-dark");
+    });
     theme_btn.classList.toggle("toggle-end");
+    sbj_info.classList.toggle("blue-900-color");
+    choice_btns.forEach(btn => {
+        btn.classList.toggle("choice-dark");
+    });
     if (document.body.classList.contains("dark-theme"))
         darktheme();
     else
         lighttheme();
-
 });
